0231de15dfcfb7c878afb8874872c6cd26438151
VedanthSinghania <148610308+VedanthSinghania@users.noreply.github.com>
2023-12-31 13:51:28 +0000

    bug fix

--- main/login/app.js
+++ main/login/app.js
@@ -1,10 +1,14 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const login_btn = document.querySelector("#Login-btn");
 
 sign_up_btn.addEventListener('click', () =>{
     container.classList.add("sign-up-mode");
 });
+login_btn.addEventListener('click',() =>{
+    console.log("hello world")
+})
 
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
